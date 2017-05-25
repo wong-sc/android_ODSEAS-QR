f39ecc9e70871f1e9cf7cc82a087065f56e8d394
voon-tw <t.wui1993@gmail.com>
2017-05-26 07:44:52 +0800

    remove unused code

--- app/src/main/java/app/app/app/odseasqr/DeviceDetailFragment.java
+++ app/src/main/java/app/app/app/odseasqr/DeviceDetailFragment.java
@@ -1,6 +1,9 @@
 package app.app.app.odseasqr;
 
+<<<<<<< HEAD
 import android.app.Activity;
+=======
+>>>>>>> remove unused code
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.ProgressDialog;
@@ -101,10 +104,10 @@ public class DeviceDetailFragment extends Fragment implements WifiP2pManager.Con
 
                     @Override
                     public void onClick(View v) {
-                            Log.d(SyncActivity.TAG, "Enter onclick");
-                            loading.setMessage("Validating course information...");
-                            loading.show();
-                            getUnsyncData();
+                        Log.d(SyncActivity.TAG, "Enter onclick");
+                        loading.setMessage("Validating course information...");
+                        loading.show();
+                        getUnsyncData();
                     }
                 });
 
